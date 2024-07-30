991e9768194c12d7b3e711da9f56e32c527f0aaa
Harish-329 <hpharish.329@gmail.com>
2024-07-30 22:45:02 +0530

    message

---
+++ src/main/java/com/movieflix/auth/entities/User.java
@@ -0,0 +1,90 @@
+package com.movieflix.auth.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @NotBlank(message = "The name field can't be blank")
+    private String name;
+
+    @NotBlank(message = "The username field can't be blank")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "The email field can't be blank")
+    @Column(unique = true)
+    @Email(message = "Please enter email in proper format!")
+    private String email;
+
+    @NotBlank(message = "The password field can't be blank")
+    @Size(min = 5, message = "The password must have at least 5 characters")
+    private String password;
+
+    @OneToOne(mappedBy = "user")
+    private RefreshToken refreshToken;
+
+    @OneToOne(mappedBy = "user")
+    private ForgotPassword forgotPassword;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
